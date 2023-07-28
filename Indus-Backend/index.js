4d083a15b8307ba075e281deec8701389d63cabc
bepratham <prathamsinghkathayat@email.com>
2023-07-28 20:07:06 +0530

    routes fixing

--- Indus-Backend/index.js
+++ Indus-Backend/index.js
@@ -96,7 +96,8 @@ server.use('/users', isAuth(), usersRouter.router);
 server.use('/auth', authRouter.router);
 server.use('/cart', isAuth(), cartRouter.router);
 server.use('/orders', isAuth(), ordersRouter.router);
-
+//
+server.get('*',(req,res)=>res.sendFile(path.resolve('build','index.html')));
 // Passport Strategies
 passport.use(
   'local',
@@ -173,7 +174,7 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 
 server.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount ,orderId } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -182,6 +183,9 @@ server.post("/create-payment-intent", async (req, res) => {
     automatic_payment_methods: {
       enabled: true,
     },
+    metadata:{
+      orderId
+    }
   });
 
   res.send({
