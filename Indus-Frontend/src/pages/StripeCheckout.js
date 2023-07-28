da43d336cff1d7289e096fc60d1f44511cfdb1d1
bepratham <prathamsinghkathayat@email.com>
2023-07-28 19:54:59 +0530

    routing bug fixed

--- Indus-Frontend/src/pages/StripeCheckout.js
+++ Indus-Frontend/src/pages/StripeCheckout.js
@@ -10,7 +10,7 @@ import { selectCurrentOrder } from "../features/order/orderSlice";
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
 // This is your test publishable API key.
-const stripePromise = loadStripe("pk_test_51N5NLVSF2Mo4AGVvYGMMNnTcPA5lHrkUv8zPymsKfL31c8m6et8525Y92zMgA22m1u6ZkkXsBofre0PJL6i3eVsh00sR6M8c00");
+const stripePromise = loadStripe("sk_test_51NYCAWSBXppglNyIhr8C7rASRViLi21aGJUYExUqwWuYxhNKEO95WTzKcPWWu1YLhW7SJ1ZAnIXDpqpRNn0CktkS00ddWFPGDT");
 
 export default function StripeCheckout() {
   const [clientSecret, setClientSecret] = useState("");
